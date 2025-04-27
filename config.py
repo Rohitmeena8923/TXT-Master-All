af3cdcc9902898fdb04212eacf5bb84e3ffc3ae9
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-04-27 15:21:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", '7671869618:AAEplVu9irkVnMbc6gCfj_7xJ65wjPhksWA')
-    API_ID = int(os.environ.get("API_ID", '29946578'))
-    API_HASH = os.environ.get("API_HASH", '57e0b762f105ab1db072fabe4d65114b')
-    AUTH_USER = os.environ.get('AUTH_USERS', '6376631545').split(',')
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", '7920749316:AAHFVE5_BfsIPjHKTdKJHnmnmu2cAfknDxY')
+    API_ID = int(os.environ.get("API_ID", '27775431'))
+    API_HASH = os.environ.get("API_HASH", 'b70bb1d45a1d05236671d4cc615e40f9')
+    AUTH_USER = os.environ.get('AUTH_USERS', '6414266397').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = "𝗥𝗼𝘆 𝗝𝗮𝗮𝘁™"#Here You Can Change with Your Name  or any custom name or title you prefer
